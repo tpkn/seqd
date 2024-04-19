cb2d0042c3395e0151242707c1361ebb5cb7faf6
tpkn <tpkn@users.noreply.github.com>
2024-04-19 03:08:06 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# ---------------------------
+# cd <installation_dir>
+# curl --silent -L https://github.com/tpkn/seqd/install.sh 2> /dev/null | sh
+# ---------------------------
+
+release_url="https://github.com/tpkn/seqd/releases/latest/download/seqd"
+
+binary_name=$(cut -d '/' -f 9 <<< "$release_url")
+binary_path="$PWD/$binary_name"
+usr_bin_path="/usr/bin"
+
+echo "Installing: $release_url ..."
+
+status=$(curl --write-out %{http_code} --silent --fail -LO "$release_url")
+if (( $status != 200 )); then
+	echo "[x] Can't download binary: $release_url"
+	exit 1
+fi
+
+echo "[✓] Downloaded: $binary_path"
+
+if ! chmod -R 0750 "$binary_path" &> /dev/null; then
+	echo "[x] Can't change permissions to '$binary_path'"
+	exit 1
+fi
+
+echo "[✓] Permissions changed to 'rwxr-x---'"
+
+while true; do
+	read -p "Create alias '$binary_name' for binary? [y/n] " q
+	case $q in
+	[Yy]* )
+			if ! command -v "$binary_name" &> /dev/null; then
+				if ! ln -s "$binary_path" "$usr_bin_path/$binary_name" &> /dev/null; then
+					echo "[x] Can't create alias '$binary_name' for '$binary_path'"
+				fi
+			else
+				echo "[-] Alias '$binary_name' already exists"
+			fi
+		break;;
+	* ) break ;;
+	esac
+done
+
+echo "[✓] Done: v$($binary_path --version)"
